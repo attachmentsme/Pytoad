a6695b1b5ec3d9bb5ab4ade88f71a2324cade0d8
bcoe <bencoe@gmail.com>
2011-05-20 12:52:10 -0400

    Updated documentation.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="Pytoad",
-      version="1.0.0",
+      version="1.0.1",
       description="Pytoad is a lightweight Hoptoad notifier for Python.",
       author="Benjamin Coe",
       author_email="ben@attachments.me",
