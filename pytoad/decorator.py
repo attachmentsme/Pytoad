733f5291e02c5699f6b38ea0d8c0bd426dfb5768
bcoe <bencoe@gmail.com>
2011-05-20 04:57:03 -0400

    Fixed small bug with cleaned up code. Unit tests pass again, and code is still significantly cleaner :)

--- pytoad/decorator.py
+++ pytoad/decorator.py
@@ -1,13 +1,15 @@
 from pytoad import Connection
 
 def pytoad_decorator(monitored_exceptions=[], PytoadConnectionClass=Connection):
-    monitored_exceptions = tuple(monitored_exceptions) or Exception
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             try:
                 f(*args, **kwargs)
-            except monitored_exceptions, e:
-                connection = PytoadConnectionClass()
-                connection.send_to_hoptoad(e)
+            except Exception, e:
+                if e.__class__ in monitored_exceptions or not monitored_exceptions:
+                    connection = PytoadConnectionClass()
+                    connection.send_to_hoptoad(e)
+                else:
+                    raise e
         return wrapped_f
     return wrap
